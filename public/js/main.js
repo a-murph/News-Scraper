9e9c19377d2398f2969dacccfeedbd07ce3c976a
Andrew Murphy <ajmurph544@gmail.com>
2018-09-13 14:13:12 -0400

    made scrape button scrape without reloading page

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,54 @@
 $(function() {
+	//scrape new articles
+	$("body").on("click", "#scrape-button", function(event) {
+		$.ajax({
+			method: "GET",
+			url: "/api/scrape"
+		}).then(function(data) {
+			if (data.length === 0) {
+				console.log("no new articles.");
+			} else {
+				//add new articles to page
+				data.forEach(function(article) {
+					//build new div for each article with all information
+					var newArticle = $("<div>");
+					newArticle.attr("class", "article");
+					newArticle.data("id", article._id);
+
+					var articleImage = $("<img>");
+					articleImage.attr("class", "article-image");
+					articleImage.attr("alt", "Article Image");
+					if (article.image) {
+						articleImage.attr("src", article.image);
+					} else {
+						articleImage.attr("src", "/assets/images/placeholder.png");
+					}
+
+					var articleTitle = $("<a>");
+					articleTitle.attr("class", "article-title");
+					articleTitle.attr("href", article.link);
+					articleTitle.text(article.title);
+
+					var articleByline = $("<p>");
+					articleByline.attr("class", "article-byline");
+					articleByline.text(article.author);
+
+					var commentButton = $("<a>");
+					commentButton.attr("href", `/${article._id}/comments`);
+					commentButton.data("id", article._id);
+
+					//add new article to top of page
+					newArticle.append(articleImage);
+					newArticle.append(articleTitle);
+					newArticle.append(articleByline);
+					newArticle.append(commentButton);
+					$(".articles-container").prepend(newArticle);
+				});
+			}
+		});
+	});
+
+	//submit comment form
 	$("body").on("click", "#form-submit", function(event) {
 		event.preventDefault();
 		var articleId = $(".article").data("id");
@@ -14,6 +64,7 @@ $(function() {
 		});
 	});
 
+	//delete comment
 	$("body").on("click", ".comment-delete", function(event) {
 		var commentId = $(this).data("id");
 
